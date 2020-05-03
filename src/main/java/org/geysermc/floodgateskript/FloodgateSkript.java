d3620f68a02d353eefae87bfab6c0079abafeb38
DoctorMacc <toy.fighter1@gmail.com>
2020-05-03 10:57:05 -0400

    Initial commit

---
+++ src/main/java/org/geysermc/floodgateskript/FloodgateSkript.java
@@ -0,0 +1,31 @@
+package org.geysermc.floodgateskript;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.SkriptAddon;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+
+
+public final class FloodgateSkript extends JavaPlugin {
+
+    private static SkriptAddon addonInstance;
+
+    @Override
+    public void onEnable() {
+        if (Bukkit.getPluginManager().getPlugin("floodgate-bukkit") != null && Bukkit.getPluginManager().getPlugin("Skript") != null) {
+            addonInstance = Skript.registerAddon(this);
+            try {
+                addonInstance.loadClasses("org.geysermc.floodgate-skript", "conditions");
+            } catch (IOException e) {
+                getLogger().warning("IOException caught. Disabling plugin.");
+                getServer().getPluginManager().disablePlugin(this);
+            }
+        } else {
+            getLogger().warning("floodgate-bukkit or Skript not found! Disabling plugin.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+    }
+}
---
+++ src/main/java/org/geysermc/floodgateskript/conditions/IsBedrockPlayer.java
@@ -0,0 +1,44 @@
+package org.geysermc.floodgateskript.conditions;
+
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.Skript;
+import ch.njol.util.Kleenean;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.geysermc.floodgate.FloodgateAPI;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Is from Floodgate")
+@Description("Condition to check if a player is a Floodgate player")
+public class IsBedrockPlayer extends Condition {
+
+    static {
+        Skript.registerCondition(IsBedrockPlayer.class, "%player% is floodgate");
+    }
+
+    private Expression<Player> player;
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "Is player from Floodgate: " + player.toString(e, debug);
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        player = (Expression<Player>) exprs[0];
+        return true;
+    }
+
+    @Override
+    public boolean check(Event e) {
+        if (player.getSingle(e) != null) {
+            return FloodgateAPI.isBedrockPlayer(player.getSingle(e));
+        }
+        return false;
+    }
+}
